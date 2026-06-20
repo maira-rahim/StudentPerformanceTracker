7fa45c5492b61681e9e354282e5f45e80d2b23ab
maira rahim <mairarahim19@gmail.com>
2026-06-20 22:19:28 +0500

    Added GPA calculation functionality

--- src/main/java/org/example/Grade.java
+++ src/main/java/org/example/Grade.java
@@ -1,4 +1,22 @@
-package org.example;
-
 public class Grade {
+
+    private double marks;
+
+    public Grade(double marks){
+        this.marks=marks;
+    }
+
+    public double calculateGPA(){
+
+        if(marks>=85)
+            return 4.0;
+
+        else if(marks>=75)
+            return 3.5;
+
+        else if(marks>=65)
+            return 3.0;
+
+        return 2.0;
+    }
 }
