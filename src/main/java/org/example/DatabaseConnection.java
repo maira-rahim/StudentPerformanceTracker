73bba22252a7d345de5f22ed34a5656fd8fca464
maira rahim <mairarahim19@gmail.com>
2026-06-20 21:31:02 +0500

    Added JDBC database connection

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -1,4 +1,27 @@
-package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DatabaseConnection {
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+
+        try {
+
+            if(connection == null){
+
+                connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/student_tracker",
+                        "root",
+                        "root"
+                );
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
