e936599e0fcc2d423924fb9e92c7328682cdcd67
maira rahim <mairarahim19@gmail.com>
2026-06-20 21:54:43 +0500

    Implemented student CRUD operations

--- src/main/java/org/example/StudentDAO.java
+++ src/main/java/org/example/StudentDAO.java
@@ -1,4 +1,24 @@
-package org.example;
+import java.sql.*;
 
 public class StudentDAO {
+
+    public void addStudent(Student student){
+
+        try{
+
+            Connection con = DatabaseConnection.getConnection();
+
+            String query =
+                    "insert into students(name,email,department) values(?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1,student.getName());
+
+            ps.executeUpdate();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
