ea72a465e54ea2fbb7bb2e3ecbaaddc15b4bd468
maira rahim <mairarahim19@gmail.com>
2026-06-20 21:35:21 +0500

    Created Student model

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,23 @@
-package org.example;
-
 public class Student {
+
+    private int id;
+    private String name;
+    private String email;
+    private String department;
+
+    public Student(int id,String name,String email,String department){
+
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.department=department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
