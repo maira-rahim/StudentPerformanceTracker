96d9f5ba8e4a088a5c6919717e7c490eac98fe02
maira rahim <mairarahim19@gmail.com>
2026-06-21 09:44:18 +0500

    Added Teacher Dashboard UI and actions

--- src/main/java/org/example/TeacherDashboard.java
+++ src/main/java/org/example/TeacherDashboard.java
@@ -1,4 +1,45 @@
-package org.example;
+import javax.swing.*;
 
-public class TeacherDashboard {
+public class TeacherDashboard extends JFrame {
+
+    public TeacherDashboard() {
+
+        JButton add = new JButton("Add Student");
+        JButton view = new JButton("View Students");
+
+        add.setBounds(50,50,150,30);
+        view.setBounds(50,100,150,30);
+
+        add(add);
+        add(view);
+
+        setSize(300,300);
+        setLayout(null);
+        setVisible(true);
+
+        add.addActionListener(e -> {
+            String name = JOptionPane.showInputDialog("Name:");
+            String course = JOptionPane.showInputDialog("Course:");
+            double marks = Double.parseDouble(JOptionPane.showInputDialog("Marks:"));
+
+            StudentDAO dao = new StudentDAO();
+            dao.addStudent(new Student(0,name,course,marks));
+        });
+
+        view.addActionListener(e -> {
+            StudentDAO dao = new StudentDAO();
+            StringBuilder sb = new StringBuilder();
+
+            for(Student s : dao.getAllStudents()) {
+                sb.append(s.getId())
+                        .append(" ")
+                        .append(s.getName())
+                        .append(" ")
+                        .append(s.getMarks())
+                        .append("\n");
+            }
+
+            JOptionPane.showMessageDialog(this, sb.toString());
+        });
+    }
 }
