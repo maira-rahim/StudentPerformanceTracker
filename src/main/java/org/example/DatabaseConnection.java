f95a1d9dcfe2391a06867dc8f338c0f1b5134fc5
maira rahim <mairarahim19@gmail.com>
2026-06-21 09:33:16 +0500

    Added Railway DB connection

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -3,25 +3,20 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
-    private static Connection connection;
+    private static final String URL =
+            "jdbc:mysql://mysql.railway.internal:3306/railway";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "rLHRsuVaGtFpCmExIHLXwWNaEkVAObQu";
 
     public static Connection getConnection() {
-
+        Connection conn = null;
         try {
-
-            if(connection == null){
-
-                connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/student_tracker",
-                        "root",
-                        "root"
-                );
-            }
-
-        } catch (Exception e){
-            e.printStackTrace();
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+        } catch (Exception e) {
+            System.out.println("DB Connection Failed: " + e.getMessage());
         }
-
-        return connection;
+        return conn;
     }
 }
