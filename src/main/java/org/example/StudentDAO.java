6030c9f9d4035c93a3729d192aefed05304718fa
maira rahim <mairarahim19@gmail.com>
2026-06-21 09:38:59 +0500

    Implemented StudentDAO CRUD operations

--- src/main/java/org/example/StudentDAO.java
+++ src/main/java/org/example/StudentDAO.java
@@ -1,24 +1,48 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class StudentDAO {
 
-    public void addStudent(Student student){
+    public void addStudent(Student s) {
+        try {
+            Connection conn = DatabaseConnection.getConnection();
 
-        try{
+            String sql = "INSERT INTO students(name, course, marks) VALUES (?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
 
-            Connection con = DatabaseConnection.getConnection();
-
-            String query =
-                    "insert into students(name,email,department) values(?,?,?)";
-
-            PreparedStatement ps = con.prepareStatement(query);
-
-            ps.setString(1,student.getName());
+            ps.setString(1, s.getName());
+            ps.setString(2, s.getCourse());
+            ps.setDouble(3, s.getMarks());
 
             ps.executeUpdate();
+            System.out.println("Student Added!");
 
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    public ArrayList<Student> getAllStudents() {
+        ArrayList<Student> list = new ArrayList<>();
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM students");
+
+            while (rs.next()) {
+                list.add(new Student(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("course"),
+                        rs.getDouble("marks")
+                ));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return list;
     }
 }
