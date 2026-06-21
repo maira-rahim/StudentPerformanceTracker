af6841794fa7dfc23b3b3ee6e96a45745897cee0
maira rahim <mairarahim19@gmail.com>
2026-06-21 09:34:58 +0500

    Updated Student class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,23 +1,27 @@
 public class Student {
-
     private int id;
     private String name;
-    private String email;
-    private String department;
+    private String course;
+    private double marks;
 
-    public Student(int id,String name,String email,String department){
+    public Student() {}
 
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.department=department;
+    public Student(int id, String name, String course, double marks) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.marks = marks;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCourse() { return course; }
+    public void setCourse(String course) { this.course = course; }
+
+    public double getMarks() { return marks; }
+    public void setMarks(double marks) { this.marks = marks; }
 }
